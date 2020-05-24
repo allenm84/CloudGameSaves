d6a7ada0292b13da954fc0ea01c66c892e799ecc
Michael Allen <michael.allen84+git@gmail.com>
2020-05-24 17:38:43 -0500

    Add ability to run at startup

--- src/CloudGameSaves/Program.cs
+++ src/CloudGameSaves/Program.cs
@@ -2,10 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32.TaskScheduler;
+using Task = System.Threading.Tasks.Task;
 
 namespace CloudGameSaves
 {
@@ -15,6 +18,8 @@ namespace CloudGameSaves
 
     public const string AppId = "8D35DEC9-F71C-4A07-B2BB-C535540D1430";
     public const string Namespace = "http://www.mapa.com/mike/cloudGameSaves";
+
+    public const string TaskName = "Startup_CloudSaves";
 
     /// <summary>
     /// The main entry point for the application.
@@ -46,29 +51,32 @@ namespace CloudGameSaves
       }
     }
 
-    public static bool RunAtStartup => !string.IsNullOrWhiteSpace(GetCurrentValue());
-
-    static string GetCurrentValue()
-    {
-      return Registry
-      .CurrentUser
-      .OpenSubKey(RegistryPath, false)
-      .GetValue(AppId, "") as string;
-    }
-
     static void SetStartup(bool add)
     {
-      var rk = Registry.CurrentUser.OpenSubKey(RegistryPath, true);
+      var service = TaskService.Instance;
+      var existingTask = service.GetTask(TaskName);
+      if (existingTask != null)
+      {
+        service.RootFolder.DeleteTask(TaskName);
+      }
 
       if (add)
       {
-        rk.SetValue(AppId, Application.ExecutablePath);
-      }
-      else
-      {
-        rk.DeleteValue(AppId, false);
+        var file = new FileInfo(Application.ExecutablePath);
+
+        var action = new ExecAction();
+        action.Path = file.FullName;
+        action.WorkingDirectory = file.Directory.FullName;
+
+        var task = service.NewTask();
+        task.Actions.Add(action);
+        task.Triggers.Add(new LogonTrigger());
+
+        service.RootFolder.RegisterTaskDefinition(TaskName, task);
       }
     }
+
+    public static bool RunAtStartup => TaskService.Instance.GetTask(TaskName)?.Enabled == true;
 
     public static void UpdateStartup(bool add)
     {
